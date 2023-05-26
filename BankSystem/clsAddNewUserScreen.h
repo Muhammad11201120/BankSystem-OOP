bbbc33c3e006d4531cf2821082d7524d69cc5b6d
mo <muha.11201120@gmail.com>
2023-05-26 20:15:28 +0300

    modifying some UI functions

--- BankSystem/clsAddNewUserScreen.h
+++ BankSystem/clsAddNewUserScreen.h
@@ -60,24 +60,22 @@ private:
 		}
 		return premission;
 	}
-	static clsUsers _readClientInfo( clsUsers& user )
+	static clsUsers _readUserInfo( clsUsers& user )
 	{
-		cout << "Enter Client First Name: ";
+		cout << "Enter User First Name: ";
 		user.setFirstName( clsInputValidate::ReadString() );
-		cout << "Enter Client Last Name: ";
+		cout << "Enter User Last Name: ";
 		user.setLastName( clsInputValidate::ReadString() );
-		cout << "Enter Client Email: ";
+		cout << "Enter User Email: ";
 		user.setEmail( clsInputValidate::ReadString() );
-		cout << "Enter Client Phone: ";
+		cout << "Enter User Phone: ";
 		user.setPhone( clsInputValidate::ReadString() );
-		cout << "Enter Client Pin Code: ";
-		user.setUserName( clsInputValidate::ReadString() );
-		cout << "Enter Account Balance: ";
+		cout << "Enter Password: ";
 		user.setPassword( clsInputValidate::ReadString() );
 		user.setPremissions( clsAddNewUserScreen::ReadUserPremission() );
 		return user;
 	}
-	static void _printClientData( clsUsers user )
+	static void _printUserData( clsUsers user )
 	{
 
 		cout << "\n\t**** CLINT: (" << user.FullName() << ") ****";
@@ -96,40 +94,40 @@ private:
 		cout << "\n--------------------------------------------";
 		cout << "\nPHONE         |" << left << setw( 12 ) << user.Phone();
 		cout << "\n--------------------------------------------";
-		cout << "\nPREMISSIONS   |" << left << setw( 12 ) << user.Premissions() << "$";
+		cout << "\nPREMISSIONS   |" << left << setw( 12 ) << user.Premissions();
 		cout << "\n--------------------------------------------\n";
 	}
 public:
 	static void addUserScreen()
 	{
 		system( "cls" );
-		_DrawScreenHeader( "ADD CLIENT SCREEN" );
+		_DrawScreenHeader( "ADD USER SCREEN" );
 		string userName = "";
-		cout << "Enter A Client Account Number: ";
+		cout << "Enter A User Name: ";
 		userName = clsInputValidate::ReadString();
 		while ( clsUsers::isUserExists( userName ) )
 		{
-			cout << "There Is An Account Already With This Account Number..\nEnter Another Account Number..";
+			cout << "There Is An Account Already With This User Name..\nEnter Another User Name..";
 			userName = clsInputValidate::ReadString();
 		}
 
 		clsUsers newUser = clsUsers::GetAddNewUserObject( userName );
 
-		_readClientInfo( newUser );
+		_readUserInfo( newUser );
 		clsUsers::enSaveResults result;
 		result = newUser.Save();
 
 		switch ( result )
 		{
 		case clsUsers::enSaveResults::svFaildEmptyObject:
-			cout << "ERRORO: No Client With That Account Number..Operation Failed.." << endl;
+			cout << "ERRORO: No User With That User Name..Operation Failed.." << endl;
 			break;
 		case clsUsers::enSaveResults::svFaildObjectAccountExists:
-			cout << "ERROR: There Is Already An Account With This Account Number.." << endl;
+			cout << "ERROR: There Is Already An Ueser With This User NAme.." << endl;
 			break;
 		case clsUsers::enSaveResults::svSucceed:
-			cout << "SUCCESS: Client Information Updated Successfully.." << endl;
-			_printClientData( newUser );
+			cout << "SUCCESS: User Information Updated Successfully.." << endl;
+			_printUserData( newUser );
 			break;
 		default:
 			break;
--- BankSystem/clsUsersList.h
+++ BankSystem/clsUsersList.h
@@ -43,7 +43,7 @@ public:
 		cout << "_________________________________________\n" << endl;
 
 		if ( vUsers.size() == 0 )
-			cout << "\t\t\t\tNo Clients Available In the System!";
+			cout << "\t\t\t\tNo Users Available In the System!";
 		else
 
 			for ( clsUsers& user : vUsers )
